444a245364178a6d261bad4a62555875e5f110cb
Fabricio Tamusiunas <fabricio@5.130.net.registro.br>
2017-07-18 15:46:48 -0300

    Acertada aquisicao do MAC Address para modelo TP-LINKTL-WR741NDv4

--- files/get_mac_address.sh
+++ files/get_mac_address.sh
@@ -3,4 +3,11 @@ mac_address=`ifconfig |grep 'br-lan '|sed s/[\ ]/'\n'/g|sed -e s/://g|egrep '^[0
 if [ "$mac_address" == "" ] ; then
   mac_address=`ifconfig |grep 'eth0 '|sed s/[\ ]/'\n'/g|sed -e s/://g|egrep '^[0-9A-Fa-f]{12}'|tr A-Z a-z`
 fi
+model=$(/usr/bin/get_model.sh)
+#
+# Acerta MAC Address para modelo TP-LINKTL-WR741NDv4
+#
+if [ "$model" = "TP-LINKTL-WR741NDv4" ] ; then
+  mac_address=$(cat /sys/devices/platform/ar933x_wmac/ieee80211/phy0/macaddress)
+fi
 echo $mac_address|sed -e s/://g|tr A-Z a-z
