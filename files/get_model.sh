0394809b32d5cf778ccde48bcebd6038584985b0
Henrique de Moraes Holschuh <henrique@nic.br>
2017-12-18 11:32:09 -0200

    [SIMB-1852] return meaningful model name (generic)
    Return meaningful names also on ARM, where "machine" is not
    present in /proc/cpuinfo.
    
    1. Use "ubus call system board" as the generic way to retrieve the
       information when machine is not found in /proc/cpuinfo.
    2. Munge spaces to _ (API change)
    3. Munge several problematic chars to _
    4. Munge repeated _ (after rules above) into a single _
    
    Tested on: ar71xx/generic (604, producao), armvirt (604)

--- files/get_model.sh
+++ files/get_model.sh
@@ -2,5 +2,31 @@
 
 . /lib/functions.sh
 
-machine=$(awk 'BEGIN{FS="[ \t]+:[ \t]"} /machine/ {print $2}' /proc/cpuinfo | sed -e 's/ //g')
+# Works well on ar71xx/generic
+machine=$(sed -n -e '/^machine[[:blank:]]/ { s/[^:]\+:[[:blank:]]\+// ; s/[ ,;#$%&\!/]/_/g ; s/_\+/_/g ; p }' < /proc/cpuinfo)
+
+# shortcut, since we will need to load in json support otherwise
+if [ -n "$machine" ] ; then
+	echo $machine
+	exit 0
+fi
+
+# do it the hard (and expensive) way
+. /usr/share/libubox/jshn.sh
+json_init
+json_load "$(ubus call system board)" || {
+	echo "unknown"
+	exit 0
+}
+json_get_var boardname board_name
+json_select release
+json_get_var target target
+
+if [ x"${target}${boardname}" != x ] ; then
+	machine=$(echo "${target}_${boardname}" | sed -e 's/[ ,;#$%&\!/]/_/g' -e 's/_\+/_/g')
+else
+	# catch-all, because things just don't work well when it is empty
+	machine="unknown"
+fi
+
 echo $machine
