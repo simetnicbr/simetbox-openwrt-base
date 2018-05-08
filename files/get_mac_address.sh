8c6d02744da2ae89e902082ef9d173dc43fe3c25
Henrique de Moraes Holschuh <henrique@nic.br>
2018-05-08 11:32:22 -0300

    [SIMB-1884] support Archer C60v2 MAC address
    Use the MAC address for eth0, as that is what is printed on the box
    label.

--- files/get_mac_address.sh
+++ files/get_mac_address.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
-mac_address=$(ifconfig |grep 'br-lan '|sed s/[\ ]/'\n'/g|sed -e s/://g | grep -E '^[0-9A-Fa-f]{12}'|tr A-Z a-z )
+mac_address=$(ifconfig | grep 'br-lan ' | sed s/[\ ]/'\n'/g | sed -e s/://g | grep -E '^[0-9A-Fa-f]{12}'|tr A-Z a-z )
 if [ -z "$mac_address" ] ; then
-	mac_address=$(ifconfig |grep 'eth0 '|sed s/[\ ]/'\n'/g|sed -e s/://g | grep -E '^[0-9A-Fa-f]{12}'|tr A-Z a-z)
+	mac_address=$(ifconfig | grep 'eth0 ' | sed s/[\ ]/'\n'/g | sed -e s/://g | grep -E '^[0-9A-Fa-f]{12}'|tr A-Z a-z)
 fi
+
+#
+# Acerta MAC Address para certos modelos
+#
 model=$(/usr/bin/get_model.sh)
-#
-# Acerta MAC Address para modelo TP-LINKTL-WR741NDv4
-#
-if [ "$model" = "TP-LINKTL-WR741NDv4" ] ; then
-  mac_address=$(cat /sys/devices/platform/ar933x_wmac/ieee80211/phy0/macaddress)
-fi
-echo "$mac_address" | sed -e "s/://g" | tr "A-Z" "a-z"
+case "$model" in
+	TP-LINK*TL-WR741NDv4)
+		mac_address=$(cat /sys/devices/platform/ar933x_wmac/ieee80211/phy0/macaddress)
+		;;
+	TP-LINK*Archer_C60_v2*)
+		mac_address=$(cat /sys/devices/platform/qca956x_wmac/ieee80211/phy1/macaddress)
+		;;
+esac
+
+echo "$mac_address" | sed -e 's/://g' | tr 'A-Z' 'a-z'
