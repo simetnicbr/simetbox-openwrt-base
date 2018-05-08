c11c00b1190f46e9f068f0d75a26fba98d90406d
Henrique de Moraes Holschuh <henrique@nic.br>
2018-05-08 11:27:23 -0300

    [SIMB-1884] modernize get_mac_address.sh
    Clean it up a bit, address shellcheck complaints

--- files/get_mac_address.sh
+++ files/get_mac_address.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-mac_address=`ifconfig |grep 'br-lan '|sed s/[\ ]/'\n'/g|sed -e s/://g|egrep '^[0-9A-Fa-f]{12}'|tr A-Z a-z` 
-if [ "$mac_address" == "" ] ; then
-  mac_address=`ifconfig |grep 'eth0 '|sed s/[\ ]/'\n'/g|sed -e s/://g|egrep '^[0-9A-Fa-f]{12}'|tr A-Z a-z`
+mac_address=$(ifconfig |grep 'br-lan '|sed s/[\ ]/'\n'/g|sed -e s/://g | grep -E '^[0-9A-Fa-f]{12}'|tr A-Z a-z )
+if [ -z "$mac_address" ] ; then
+	mac_address=$(ifconfig |grep 'eth0 '|sed s/[\ ]/'\n'/g|sed -e s/://g | grep -E '^[0-9A-Fa-f]{12}'|tr A-Z a-z)
 fi
 model=$(/usr/bin/get_model.sh)
 #
@@ -10,4 +10,4 @@ model=$(/usr/bin/get_model.sh)
 if [ "$model" = "TP-LINKTL-WR741NDv4" ] ; then
   mac_address=$(cat /sys/devices/platform/ar933x_wmac/ieee80211/phy0/macaddress)
 fi
-echo $mac_address|sed -e s/://g|tr A-Z a-z
+echo "$mac_address" | sed -e "s/://g" | tr "A-Z" "a-z"
