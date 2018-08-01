86d759f229436b6bd03c3b452ef7d9de93b58581
Henrique de Moraes Holschuh <henrique@nic.br>
2018-08-01 13:49:19 -0300

    [MDC-155] get_mac_address: handle many exceptions
    Several routers have the MAC of one of the WLAN interfaces in the label
    instead of the one that ends up in br-lan.  We need to compensate for
    that.
    
    Without this, the MAC address of the SIMET probe could change, losing
    previous history if it is ever factory-erased.  Also, any "defaults"
    that are supposed to match the MAC address in the label, wouldn't.

--- files/get_mac_address.sh
+++ files/get_mac_address.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
-mac_address=$(ifconfig | grep 'br-lan ' | sed s/[\ ]/'\n'/g | sed -e s/://g | grep -E '^[0-9A-Fa-f]{12}'|tr A-Z a-z )
-if [ -z "$mac_address" ] ; then
-	mac_address=$(ifconfig | grep 'eth0 ' | sed s/[\ ]/'\n'/g | sed -e s/://g | grep -E '^[0-9A-Fa-f]{12}'|tr A-Z a-z)
-fi
 
-#
-# Acerta MAC Address para certos modelos
-#
+# get_mac_address.sh result should match the MAC address printed in the device label
 model=$(/usr/bin/get_model.sh)
 case "$model" in
-	TP-LINK*TL-WR741NDv4)
+	TP-LINK*TL-WR740N*v4)
 		mac_address=$(cat /sys/devices/platform/ar933x_wmac/ieee80211/phy0/macaddress)
 		;;
-	TP-LINK*Archer_C60_v2*)
+	TP-LINK*Archer_C60_v2)
 		mac_address=$(cat /sys/devices/platform/qca956x_wmac/ieee80211/phy1/macaddress)
 		;;
+	TP-LINK*TL-WR740ND|TP-LINK*TL-WR741N*v4|TP-LINK*TL-WR841N*v7|TP-LINK*TL-WR842N*v1)
+		mac_address=$(cat /sys/devices/pci0000:00/0000:00:00.0/ieee80211/phy0/macaddress)
+		;;
+	TP-LINK*TL-WDR3600/4300/4310)
+		mac_address=$(cat /sys/devices/pci0000:00/0000:00:00.0/ieee80211/phy1/macaddress)
+		;;
+	*)
+		mac_address=$(ifconfig | grep 'br-lan ' | sed s/[\ ]/'\n'/g | sed -e s/://g | grep -E '^[0-9A-Fa-f]{12}'|tr A-Z a-z )
+		if [ -z "$mac_address" ] ; then
+			mac_address=$(ifconfig | grep 'eth0 ' | sed s/[\ ]/'\n'/g | sed -e s/://g | grep -E '^[0-9A-Fa-f]{12}'|tr A-Z a-z)
+		fi
 esac
 
-echo "$mac_address" | sed -e 's/://g' | tr 'A-Z' 'a-z'
+echo "$mac_address" | tr -d : | tr 'A-Z' 'a-z'
