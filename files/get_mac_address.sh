e15b6eb55ce5281648664d23ad30ec460ce1e3af
Henrique de Moraes Holschuh <henrique@nic.br>
2018-08-01 16:05:59 -0300

    [MDC-155] get_mac_address: case-insensitive match for model name
    * Depeding on the source of the model name, it can change case.
      Forward-proof by forcing lowercase to uppercase before compare.
    
    * Optimize a few code paths while at it

--- files/get_mac_address.sh
+++ files/get_mac_address.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
 # get_mac_address.sh result should match the MAC address printed in the device label
-model=$(/usr/bin/get_model.sh)
+model=$(/usr/bin/get_model.sh | tr a-z A-Z)
 case "$model" in
-	TP-LINK*TL-WR740N*v4)
+	TP-LINK*TL-WR740N*V4)
 		mac_address=$(cat /sys/devices/platform/ar933x_wmac/ieee80211/phy0/macaddress)
 		;;
-	TP-LINK*Archer_C60_v2)
+	TP-LINK*ARCHER_C60_V2)
 		mac_address=$(cat /sys/devices/platform/qca956x_wmac/ieee80211/phy1/macaddress)
 		;;
-	TP-LINK*TL-WR740ND|TP-LINK*TL-WR741N*v4|TP-LINK*TL-WR841N*v7|TP-LINK*TL-WR842N*v1)
+	TP-LINK*TL-WR740ND|TP-LINK*TL-WR741N*V4|TP-LINK*TL-WR841N*V7|TP-LINK*TL-WR842N*V1)
 		mac_address=$(cat /sys/devices/pci0000:00/0000:00:00.0/ieee80211/phy0/macaddress)
 		;;
 	TP-LINK*TL-WDR3600/4300/4310)
 		mac_address=$(cat /sys/devices/pci0000:00/0000:00:00.0/ieee80211/phy1/macaddress)
 		;;
 	*)
-		mac_address=$(ifconfig | grep 'br-lan ' | sed s/[\ ]/'\n'/g | sed -e s/://g | grep -E '^[0-9A-Fa-f]{12}'|tr A-Z a-z )
+		mac_address=$(ifconfig | grep 'br-lan ' | sed s/[\ ]/'\n'/g | sed -e s/://g | grep -E '^[0-9A-Fa-f]{12}')
 		if [ -z "$mac_address" ] ; then
-			mac_address=$(ifconfig | grep 'eth0 ' | sed s/[\ ]/'\n'/g | sed -e s/://g | grep -E '^[0-9A-Fa-f]{12}'|tr A-Z a-z)
+			mac_address=$(ifconfig | grep 'eth0 ' | sed s/[\ ]/'\n'/g | sed -e s/://g | grep -E '^[0-9A-Fa-f]{12}')
 		fi
 esac
 
-echo "$mac_address" | tr -d : | tr 'A-Z' 'a-z'
+echo "$mac_address" | tr -d : | tr A-Z a-z
