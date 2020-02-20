9a26fbeba00c63f512ebe15c2c3bc284adc6af07
Henrique de Moraes Holschuh <henrique@nic.br>
2020-02-20 14:36:53 -0300

    [MDC-791] simetbox-openwrt-base: move hash info to /etc/simet
    * Use /etc/simet/simet1_hash instead of /etc/config/hash
    * Use /etc/simet/simet1_deviceid instead of
      /etc/config/simetbox_hash_configured
    * Add an uci-defaults script to remove /etc/config/hash and
      /etc/config/simetbox_hash_configured
    
    We *want* to force a recalculation of the device hash (to get the new
    hash), which the above acomplishes. It will also trigger the SIMET1
    registration (to update the hash in the database).
    
    This will happen only once (or as first_install), since /etc/simet/*
    will be preserved by sysupgrade.

--- files/simetbox_register.sh
+++ files/simetbox_register.sh
@@ -3,34 +3,36 @@
 . /etc/config/simet.conf
 
 hash_device=$(/usr/bin/get_mac_address.sh)
-
-hash=$(cat /etc/config/hash)
-
+hash=$(cat /etc/simet/simet1_hash)
 model=$(get_model.sh)
-
 version=$(get_simet_box_version.sh)
+
+[ -z "$hash" ] && {
+	echo "simetbox_register.sh: simet1_hash missing. cannot proceed" >&2
+	exit 1
+}
 
 parametros="{\"macAddress\": \"$hash_device\",\"model\": \"$model\",\"currentVersion\":\"$version\",\"hash\":\"$hash\"}"
 
 force=
 # detecta troca de MAC por qualquer motivo (por exemplo, reconfiguração de quem é br-lan)
 # caso contrário, o sistema SIMET irá recusar serviço à caixa.
-if [ -r /etc/config/simetbox_hash_configured ] ; then
-  read -r oldhashdevice oldversion < /etc/config/simetbox_hash_configured
+if [ -r /etc/simet/simet1_deviceid ] ; then
+  read -r oldhashdevice oldversion < /etc/simet/simet1_deviceid
   if [ "x${hash_device}" != "x${oldhashdevice}" ]; then
      echo "simetbox_register.sh: WARNING: main device MAC address changed, registering again..." >&2
-     rm -f /etc/config/simetbox_hash_configured
+     rm -f /etc/simet/simet1_deviceid
   elif [ "x${version}" != "x${oldversion}" ]; then
      echo "simetbox_register.sh: version change detected, updating registration..." >&2
      force=1
   fi
 fi
 
-while [ ! -f /etc/config/simetbox_hash_configured ] || [ -n "$force" ]
+while [ ! -f /etc/simet/simet1_deviceid ] || [ -n "$force" ]
 do 
   output_send_hash=$(simet_ws -p "https://$cf_host/$cf_simet_web_services_optional/resources/simetbox" "$parametros" | grep "\"success\":true")
   if [ "$output_send_hash" != "" ] ; then
-    echo "${hash_device} ${version}" > /etc/config/simetbox_hash_configured
+    echo "${hash_device} ${version}" > /etc/simet/simet1_deviceid
     force=
   else
     sleep 60
