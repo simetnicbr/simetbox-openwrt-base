6d6a3636f01872fa4a0db8e49177e595911b4aa7
Henrique de Moraes Holschuh <henrique@nic.br>
2020-02-20 10:35:29 -0300

    [MDC-791] simetbox-openwrt-base: trigger legacy register on version change
    Track the simetbox version and trigger a legacy register webservice call
    should it change.  This makes it much easier to deal with changes on the
    hash and also helps ensure the version is up-to-date in the legacy
    database.

--- files/simetbox_register.sh
+++ files/simetbox_register.sh
@@ -12,21 +12,26 @@ version=$(get_simet_box_version.sh)
 
 parametros="{\"macAddress\": \"$hash_device\",\"model\": \"$model\",\"currentVersion\":\"$version\",\"hash\":\"$hash\"}"
 
+force=
 # detecta troca de MAC por qualquer motivo (por exemplo, reconfiguração de quem é br-lan)
 # caso contrário, o sistema SIMET irá recusar serviço à caixa.
 if [ -r /etc/config/simetbox_hash_configured ] ; then
-  read -r oldhashdevice < /etc/config/simetbox_hash_configured
+  read -r oldhashdevice oldversion < /etc/config/simetbox_hash_configured
   if [ "x${hash_device}" != "x${oldhashdevice}" ]; then
      echo "simetbox_register.sh: WARNING: main device MAC address changed, registering again..." >&2
      rm -f /etc/config/simetbox_hash_configured
+  elif [ "x${version}" != "x${oldversion}" ]; then
+     echo "simetbox_register.sh: version change detected, updating registration..." >&2
+     force=1
   fi
 fi
 
-while [ ! -f /etc/config/simetbox_hash_configured ] 
+while [ ! -f /etc/config/simetbox_hash_configured ] || [ -n "$force" ]
 do 
   output_send_hash=$(simet_ws -p "https://$cf_host/$cf_simet_web_services_optional/resources/simetbox" "$parametros" | grep "\"success\":true")
   if [ "$output_send_hash" != "" ] ; then
-    echo "${hash_device}" > /etc/config/simetbox_hash_configured
+    echo "${hash_device} ${version}" > /etc/config/simetbox_hash_configured
+    force=
   else
     sleep 60
   fi
